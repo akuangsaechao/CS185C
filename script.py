f8a6cc5efa01b45ce5fef6836297ad5fc78ff3c7
Victor <victorfateh@yahoo.com>
2017-11-25 20:35:57 -0800

    Count's Unique Buyer
    Just a tiny method to practice iterating through all 58,000+ entries.
    Sums all unique buyer IDs and uses tqdm to display progress.

--- script.py
+++ script.py
@@ -1,9 +1,27 @@
-import csv
 import sys
 import pandas as pd
-import numpy as np
+from tqdm import tqdm
 
-df = pd.read_csv(sys.argv[1])
-print(df)
+# Allows all 8 columns to be displayed in cli
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
-print("exit successful")
+
+def unique_buyer(arg):
+    df = pd.read_csv(arg)
+
+    add = []
+
+    with tqdm(total=len(df.index)) as pbar:
+        for index, row in df.iterrows():
+            # print(row['UnitPrice'], row['CustomerID'])
+            if row['UnitPrice'] == 4.15:
+                add.append(row['CustomerID'])
+            pbar.update(1)
+
+    print(str(len(set(add))) + " Unique Buyers")
+
+
+unique_buyer(sys.argv[1])
