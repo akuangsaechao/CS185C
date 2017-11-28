d73e6898d88655386bd0f2c0134b3e028acb38c7
Victor <victorfateh@yahoo.com>
2017-11-27 17:35:42 -0800

    Overwrite conflict

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import sys
-<<<<<<< HEAD
 import pandas as pd
 from tqdm import tqdm
 from datetime import datetime
@@ -81,35 +80,3 @@ print(unique_buyer(sys.argv[1]))
 
 #print(pivot_product(sys.argv[1]))
 #print_table(sys.argv[1])
-
-=======
-import pandas
-from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-
-dataset = pandas.read_csv(sys.argv[1])
-# shape
-print(dataset.shape)
-
-# head
-print(dataset.head(20))
-
-# descriptions
-print(dataset.describe())
-
-# class distribution
-print(dataset.groupby('CustomerID'))
-
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
->>>>>>> origin/master
