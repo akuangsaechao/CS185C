b64497d169e6b8e9f2b2d1936e26286d9b32d80f
Victor <victorfateh@yahoo.com>
2017-11-27 17:34:02 -0800

    New functions
    Pivot table and other helper methods

--- script.py
+++ script.py
@@ -1,14 +1,20 @@
 import sys
 import pandas as pd
 from tqdm import tqdm
+from datetime import datetime
 
-# Allows all 8 columns to be displayed in cli
+import numpy as np
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+# Modify CLI Output of Data frame
 pd.set_option('display.height', 1000)
 pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_columns', 7)
 pd.set_option('display.width', 1000)
 
 
+# Sum of unique buyer ID
 def unique_buyer(arg):
     df = pd.read_csv(arg)
 
@@ -17,11 +23,61 @@ def unique_buyer(arg):
     with tqdm(total=len(df.index)) as pbar:
         for index, row in df.iterrows():
             # print(row['UnitPrice'], row['CustomerID'])
-            if row['UnitPrice'] == 4.15:
+            if row['UnitPrice'] == 9999999:
                 add.append(row['CustomerID'])
             pbar.update(1)
 
-    print(str(len(set(add))) + " Unique Buyers")
+    return str(len(set(add))) + " Unique Buyers"
 
 
-unique_buyer(sys.argv[1])
+# Print current data frame
+def print_table(arg):
+    print(pd.read_csv(arg))
+
+
+# return pandas dataframe of csv
+def csv_dataframe(arg):
+    return pd.read_csv(arg)
+
+
+# Converts excel float time to datetime object
+def time_converter(serial):
+    seconds = (serial - 25569) * 86400.0
+    return datetime.utcfromtimestamp(seconds)
+
+
+# Table where customerID are row and each column is a different product code
+# Values for the table are the quantity purchased of specified product
+def pivot_product(arg):
+    df = pd.read_csv(arg)
+    table = df.pivot_table(index=["CustomerID"], columns=["UnitPrice"], values="Quantity")
+    table = table.fillna(0)
+    return table
+
+
+X = [[100, 10],[5, 50], [10, 100]]
+Y = []
+
+for index, x in enumerate(X):
+    if x[0] >= 50:
+        if x[1] >= 5:
+            Y.append('rich')
+        else:
+            Y.append('poor more 50')
+    else:
+        if x[1] >= 5:
+            Y.append('rich less 50')
+        else:
+            Y.append('poor')
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+#print(clf.predict([[80, 10]]))
+
+#print(csv_dataframe(sys.argv[1]))
+
+print(unique_buyer(sys.argv[1]))
+
+#print(pivot_product(sys.argv[1]))
+#print_table(sys.argv[1])
+
